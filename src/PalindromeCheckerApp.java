04829011e379c4f12276eff46419ab30052e2498
Kageyama19 <tc4369@srmist.edu.in>
2026-02-20 09:49:36 +0530

    UC2: code for palindrome

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,2 +1,15 @@
-public class PalindromeCheckerApp {
+public static boolean  PalindromeCheckerApp(String str) {
+    for(int i=0;i<str.length()/2;i++){
+        int n= str.length();
+        if(str.charAt(i) != str.charAt(n-1-i)){
+            //not a palindrome
+            return false;
+
+        }
+    }
+    return true;
+}
+public static void main (String[] args){
+    String str= "madam";
+    System.out.println(PalindromeCheckerApp(str));
 }
