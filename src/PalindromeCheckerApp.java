e7a59ab19a2e0add962dca74ace9d82dc7c83ca7
Kageyama19 <tc4369@srmist.edu.in>
2026-02-20 10:34:07 +0530

    UC1: Add welcome message module

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,15 +1,25 @@
-public static boolean  PalindromeCheckerApp(String str) {
-    for(int i=0;i<str.length()/2;i++){
-        int n= str.length();
-        if(str.charAt(i) != str.charAt(n-1-i)){
-            //not a palindrome
-            return false;
+public class PalindromeCheckerApp{
+    static final String version = "1.0";
+    public static void main(String[] args){
+        System.out.println("Welecone to the palindrome Checker Application");
+        System.out.println("Version"+ version);
+        System.out.println("System Initialized Successfully");
 
-        }
     }
-    return true;
+
 }
-public static void main (String[] args){
-    String str= "madam";
-    System.out.println(PalindromeCheckerApp(str));
-}
+//public static boolean  PalindromeCheckerApp(String str) {
+//    for(int i=0;i<str.length()/2;i++){
+//        int n= str.length();
+//        if(str.charAt(i) != str.charAt(n-1-i)){
+//            //not a palindrome
+//            return false;
+//
+//        }
+//    }
+//    return true;
+//}
+//public static void main (String[] args){
+//    String str= "madam";
+//    System.out.println(PalindromeCheckerApp(str));
+//}
